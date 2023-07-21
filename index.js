ac8fbe212a012b33bb6f73615aef034a75ba9508
Glitch (glitch-hello-node) <none>
2023-07-21 17:34:28 +0000

    🍰💰 Checkpoint ./index.js:56850320/879 ./package.json:56850320/8198

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+var admin = require("firebase-admin");
+
+// var serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+
+console.log(process.env.FIREBASE_SERVICE_ACCOUNT)
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
 
 
 
